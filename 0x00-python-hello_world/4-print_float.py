69d7946ed319f1b9d4a9243cadddbf84cc75af7d
Chikadike <dikechika87@gmail.com>
2023-01-02 16:12:17 -0800

    python

---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+number = 3.14159
+number = float(number)
+format_float = "{:.2f}".format(number)
+print("float: {}".format(format_float)) 
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = (a[0:2])
+word_last_2 = (a[7:])
+middle_word = (a[4:-4])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
+
